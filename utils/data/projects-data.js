1a32a513b6834373f21fc3128a7d3e06d0698c6a
Vansh Nagpal <2002vansh@gmail.com>
2024-12-15 14:53:17 -0500

    added Roost AI project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,6 +6,16 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
   {
     id: 1,
+    name: 'RpostAI - A University-Centric Chatbot',
+    description: "RoostAI is a Retrieval-Augmented Generation (RAG) chatbot designed for the University of South Carolina community. By combining web-scraping, natural language processing, and machine learning, the chatbot provides precise, context-aware responses to user queries. Our evaluation shows RoostAI outperforms standard large language models in semantic and factual accuracy, offering an innovative solution to accessing campus information.",
+    tools: ['Python', 'LLMs', "ChromaDB", 'Playwright', 'Selenium', 'RAGAS'],
+    role: '[Data Engineer, ML Engineer] ',
+    code: '',
+    demo: 'http://129.114.26.208:8501/',
+    image: crefin,
+  },
+  {
+    id: 2,
     name: 'Aqua Sense - A Water Quality Assessment Chatbot',
     description: "Water quality assessment is critical in public health and environmental science. My project addresses the question: 'How safe is water from a specific source to drink?' Using the USGS REST API, the program analyzes real-time water data like pH, Dissolved Oxygen, and Turbidity. By implementing an RBF-based approach and SVM classification model, it determines water potability as a percentage. I enhanced the project with an NLP chatbot using Rasa, BERT, and NLTK to enable natural user interactions about water safety.",
     tools: ['Python', 'RASA Chatbot', 'USGS API', 'NLP', 'Supervised ML',],
@@ -15,7 +25,7 @@ export const projectsData = [
     image: crefin,
   },
   {
-    id: 2,
+    id: 3,
     name: 'Learning Management System(LMS)',
     description: "Recognizing the growing importance of programming skills, my team developed a Learning Management System (LMS) that helps students learn Python or JavaScript. Built in Java using object-oriented principles and SCRUM methodology, I focused on back-end development. My key contribution was implementing data management functionality, enabling seamless JSON-based storage of grades, classes, and course content. The project provided valuable insights into the Software Development Life Cycle.",
     tools: ['Java', 'Python', 'JavaScript', 'SCRUM', 'Software Dev. Life Cycle (SDLC)',],
@@ -25,7 +35,7 @@ export const projectsData = [
     image: realEstate,
   },
   {
-    id: 3,
+    id: 4,
     name: 'Nerdle',
     description: "Inspired by Wordle, I developed my own word-guessing game called 'Nerdle' (named after my brother Neil). I initially created a command-line version in Java to understand game mechanics, then transitioned to a web application using JavaScript, HTML, and CSS. The project helped me explore data structures like dictionaries and tries, while providing a hands-on introduction to web development.",
     tools: ['Vanilla JS', 'Java', 'HTML/CSS',],
